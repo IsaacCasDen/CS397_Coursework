a1ccc24dc2a0e68fd272e657e27c727594c048e0
Isaac Casteel-Denney <isaaccasden@cd-08-l.home>
2019-01-29 23:30:41 -0700

    Added coding challenge 3

--- codechallenge/2018-01-23/challenge3.c
+++ codechallenge/2018-01-23/challenge3.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
-#include <std
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+
+    if (argc!=2) {
+        fprintf(stderr,"usage: ./aout fileName\n");
+        exit(1);
+    }
 
     FILE *fptr;
 
     fptr = fopen(argv[1],w="w");
 
     fprintf(fptr,"Fred 42\n");
+    fclose(fptr);
+
+    fptr=fopen(argv[1],"r");
+    char name[40];
+    int age;
+
+    fscanf(fptr, "%s",name);
+    fscanf(fptr,"%d",&age);
+
+    printf("read in: %s, %d\n", name, age);
 }
