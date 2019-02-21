e31a86a96a1a715990c82fdd2590b7c62cf60c10
Isaac Casteel-Denney <isaaccasden@cd-08-l.wsc.western.edu>
2019-02-20 22:02:50 -0700

    Homework 05 - Broken

--- homework/hw05/mTar.cpp
+++ homework/hw05/mTar.cpp
@@ -11,25 +11,30 @@ const char *FILE_UNTAR = "-x";
 
 struct filedata {
 
-    size_t fileNameSize;
-    size_t fileStatSize;
-    size_t byteLength;
-    
+    int fileNameSize;
     char *fileName;
-    struct stat *fileStat;
-    char *bytes;
     
+    int fileStatSize;
+    struct stat fileStat;
+    
+    int byteLength;
+    char *bytes;
 };
+    
 
 int tarFiles(char *filepath, int argc, char **argv);
+int untarFiles(char *filepath);
+
+int writeStruct(FILE *file, struct filedata *inStruct);
+int readStruct(FILE *file, struct filedata *outStruct);
 
 int main(int argc, char **argv) {
-
+    
     if (argc>2) {
         if (strcmp(argv[1],FILE_TAR)==0 && argc>3) {
             tarFiles(argv[2],argc-3,argv+(3*sizeof(char)));
         } else if (strcmp(argv[1],FILE_UNTAR)==0) {
-            //untarFiles(argv[2]);
+            untarFiles(argv[2]);
         } else {
             return -1;
         }
@@ -41,8 +46,6 @@ int main(int argc, char **argv) {
 }
 
 int tarFiles(char *filepath, int argc, char **argv) {
-
-    char *filestream;
     int fileCount=0;
     
     for (int i=0; i<argc; i++) {
@@ -69,30 +72,75 @@ int tarFiles(char *filepath, int argc, char **argv) {
         FILE *inFile = fopen(argv[i],"rb");
         struct filedata *filedata = (struct filedata *) malloc(sizeof(struct filedata));
 
-        filedata->fileNameSize=sizeof(*argv[i]);
-        filedata->fileName=argv[i];
+        filedata->fileNameSize=strlen(argv[i]);
+        filedata->fileName=strdup(argv[i]);
+
+        //printf("Filename: %s\n",filedata->fileName);
+        printf("Filename Size: %d\n",filedata->fileNameSize);
 
         filedata->fileStatSize=sizeof(*statdata);
         filedata->fileStat=statdata;
 
         filedata->byteLength=statdata->st_size;
-        filedata->bytes=(char *) malloc(filedata->byteLength*sizeof(char));
-        fread(filedata->bytes,statdata->st_size,1,inFile);
-        fwrite(filedata,sizeof(struct filedata),1,outFile);
+        //filedata->bytes=(char *) malloc(filedata->byteLength*sizeof(char));
+        //fread(filedata->bytes,filedata->byteLength,1,inFile);
+        writeStruct(outFile,filedata);
+        fclose(inFile);
     }
+    
+    fclose(outFile);
 
     return 0;
 }
 
-/*
 int untarFiles(char *filepath) {
+    int fileCount=0;
 
-}
+    FILE *inFile = fopen(filepath, "rb");
+    fread(&fileCount,sizeof(int),1,inFile);
 
-int addFile(char *filename) {
+    struct filedata **files = (struct filedata **) malloc(fileCount*sizeof(struct filedata));
+    for (int i=0; i<fileCount; i++) {
+        files[i]=(struct filedata *) malloc(sizeof(struct filedata));
+        readStruct(inFile,files[i]);
 
-}
-int extractFile() {
+        printf("File found: \n");
+        printf("Size: %d\n",files[i]->byteLength);
+        printf("Name Length: %d\n",files[i]->fileNameSize);
+        printf("Name: %s\n",files[i]->fileName);
+        printf("Date: %s",files[i]->fileStat->st_ctim);
+        
+    }
+    
 
-}
+/*
+    for (int i=0; i<fileCount; i++) {
+        printf("Reading file: %d\n",i);
+        struct filedata *filedata = (struct filedata *) malloc(sizeof(struct filedata));
+        readStruct(inFile,filedata);
+        
+
+        
+        //printf("Byte Length: %d\n",filedata->byteLength);
+        //printf("Bytes: %c\n", filedata->bytes);
+        //printf("Stat Size: %d\n", filedata->fileStatSize);
+    }
 */
+    fclose(inFile);
+
+    return 0;
+}
+
+int writeStruct(FILE *file, struct filedata *inStruct) {
+
+    fwrite(inStruct,sizeof(struct filedata),1,file);
+    
+    return 0;
+}
+
+int readStruct(FILE *file, struct filedata *outStruct) {
+
+    fread(outStruct,sizeof(struct filedata),1,file);
+
+    return 0;
+}
